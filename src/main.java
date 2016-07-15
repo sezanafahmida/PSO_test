a64b66a4f83ea60db38ab39862a8636eff12943a
Sezana Fahmida <Sezana Fahmida>
2016-07-15 16:34:36 +0600

    Test data, partial fitness assess function added

--- src/main.java
+++ src/main.java
@@ -6,8 +6,10 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         Representation_test rp=new Representation_test(2, 5);
+         Representation_test rp=new Representation_test(2, 8);
          rp.init();
+         rp.input();
+         rp.fitness_asses();
 	}
 
 }
